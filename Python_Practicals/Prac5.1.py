dd09365af99ca942a5c3755fce72ae915e18400c
Dhiraj Madhukar Jagtap <dhirajdj30@gmail.com>
2022-03-11 23:15:56 +0530

    Uploaded Prac5

---
+++ Python_Practicals/Prac5.1.py
@@ -0,0 +1,8 @@
+# Write a program to calculate the area and the perimeter of the square
+Length = int(input("Enter the lenght of the square:"))
+
+Breadth = int(input("Enter the breadth of the square:"))
+
+print("The Area of the given Square:",(Length*Breadth))
+
+print("The Perimeter of the Square:",((2*Length)+(2*Breadth)))
---
+++ Python_Practicals/Prac5.2.py
@@ -0,0 +1,11 @@
+# Write a program to calculate perimeter and volume of the cone
+from cmath import pi
+
+
+Height = int(input("Enter Height of the Cone: "))
+Radius = int(input("Enter Radius of the Cone: "))
+Perimeter = 2*pi*Radius
+print("Perimeter of the Cone is : ", Perimeter)
+Volume = (1/3)*pi*(Radius*Radius)*Height
+print("Volume of the Cone is : ",Volume)
+# 1/3pir2 h
